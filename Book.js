32b132b891dcc45e97cef3ad88accc9cf0b7ce80
Daniel Maldonado <damaldy19@hotmail.com>
2022-05-01 22:14:06 -0500

    ShoppingCart class & addBook method done

--- Book.js
+++ Book.js
@@ -10,6 +10,7 @@ class Book{
         /*"this" word refers directly to the object property. Thats how we assign a value to the referred property when we create a new object instance. "_" symbol means the property is private and it shouldnt be modified.*/   
         }
         /*Abstraction: pick up ONLY the useful properties of a object. It only focus in important details*/
+    
     }
     /*Any class has no return. It only contains variables and functions (properties & methods).
     */
---
+++ ShoppingCart.js
@@ -0,0 +1,22 @@
+class Shoppingcart{
+    /*This class will allows to load the books we want to buy. It will sum the total amount of the cart and finally, it will display the total amount we're willing to pay.*/
+
+    constructor(){
+        //Constructor has no parameters because we're not going to insert anyone.
+        this.products = [];
+        //An empty array which we'll fill as we add books by the "addBook" method.
+        
+    }
+
+    addBook(quant, price){
+        this.products.push(Array(quant).fill(price));
+        //"this.products" is an dynamic array.
+        //".push()"" is the method which allow us to add elements to "this.products" array.
+        //"Array()" is an object, "(quant)" creates the number of empty spaces.
+        //".fill()" allow us to put an element in the array selected position. "(price)" is the element we're going to put in the position.
+
+    }
+}
+
+const myCart = new Shoppingcart();
+myCart.addBook(2,100);
